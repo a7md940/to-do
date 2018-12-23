f115f042245713f38279244022ba0c147f8ef8f6
a7md940 <ahmedatefangelov@gmail.com>
2018-12-24 01:27:22 +0200

    finish profile page style.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { UpdateImageService } from '../services/user/update-image.service';
 import { HttpEventType } from '@angular/common/http';
@@ -9,18 +9,21 @@ import { IsloggedinService } from '../services/isloggedin.service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, AfterViewInit {
 
   allTodos;
   completedTodos;
-
   userImage;
-
   progressBarPercentage;
-  constructor(public profile: UpdateImageService, public loggedIn: IsloggedinService) { }
+  showImageForm: boolean = false;
+  invalidFileTypeErr: boolean = false;
+
+  constructor(
+    public profile: UpdateImageService, 
+    public loggedIn: IsloggedinService
+    ) { }
 
   ngOnInit() {
-
     this.profile.getUserTodos().subscribe( (res:any)=>{
       console.log(res);
       this.allTodos = res.allTodos;
@@ -28,18 +31,35 @@ export class ProfileComponent implements OnInit {
     })
   }
 
+  ngAfterViewInit(){
+    // upload image file vanilla js code.
+    
+  }
+
   updateImageProfile(e, image){
     e.preventDefault();
-    const file = image.files[0];
-    console.log(file);
 
+    // call update user image service.
     this.profile.updateUserImage(image.files[0])
     .subscribe( event => {
-      console.log(event)
+
+      // if the HttpEventType is upload progress.
       if(event.type === HttpEventType.UploadProgress){
+        // increase progress bar with the percentage.
         this.progressBarPercentage = Math.round((event.loaded / event.total) * 100) + '%';
-      }else if ( event.type === HttpEventType.Response){
-        localStorage.setItem('userImage', event.body.user.userImage); 
+      }
+      // if the HttpEventType is the final response.
+      else if ( event.type === HttpEventType.Response){
+        
+        // store the image url in the local storage.
+        localStorage.setItem('userImage', event.body.user.userImage);
+
+        // if image successfully uploaded in the server, close the form after 2 seconds.
+        if(event.body.success)
+          return setTimeout(() => {
+            // close the form.
+            this.showImageForm = false;
+          }, 2000);
       }
     },
     err => console.log(err));
@@ -47,9 +67,25 @@ export class ProfileComponent implements OnInit {
 
   onInputChange(image){
     const file = image.files[0];
+    
+    // check if the file type is an image type.
+    if(!/png|jpg|jpeg|gif/i.test(file.type))
+    { 
+      // show error message.
+      this.invalidFileTypeErr = true;
+      setTimeout(()=>{
+        // close the form window after 3 seconds.
+        this.invalidFileTypeErr = false;
+      }, 3000);
+      // return.
+      return;
+    }
+
+    // if file type is valid, read the file locally in browser.
     const reader = new FileReader();
     reader.readAsDataURL(file)
     reader.onload = ()=>{
+      // put the result in a field to render in html template.
       this.userImage = reader.result;
     }
   }
